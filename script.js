e7df7a5c839e90897016f35173b4d45c66a24e84
Deva-220602 <sundardevasena007@gmail.com>
2026-06-28 14:26:25 +0530

    my new changes

--- script.js
+++ script.js
@@ -210,3 +210,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
+ 
---
+++ work.js
@@ -0,0 +1,10 @@
+var sidenav = document.querySelector(".side-bar");
+
+function showSidebar() {
+    sidenav.style.display="block";
+}
+ 
+function closeSidebar(){
+    sidenav.style.display="none";
+}
+
